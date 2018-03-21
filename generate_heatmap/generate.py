5ad68e9b729a7322d0e3129beb1c9214d51ea7a8
Ajay Rawat <ajaycl61@gmail.com>
2018-03-21 20:47:28 +0530

    cmds for ffmpeg added

--- generate_heatmap/generate.py
+++ generate_heatmap/generate.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+# import subprocess
 # import skvideo.io
 
 
@@ -10,31 +11,31 @@ def preprocess(img):
 
 
 def generate_images(vloc, imgloc, cwd=None, skp=None):
-    # i = 0
+    i = 0
     for videos in sorted([x for x in os.listdir(vloc) if not x.startswith('.')],
                          key=lambda x: x.lower()):
         print("Processing Video {}".format(videos))
 
-        os.chdir(cwd)
+        # os.chdir(cwd)
         # "ffmpeg -i video.webm image-%03d.png"
 
-        subprocess.call(['ffmpeg', '-i', 'image-%03d.png', vloc])
+        # subprocess.call(['ls', '-l'])
         # subprocess.call(['ffmpeg', '-i', 'output.avi', '-t', '5', 'out.gif'])
 
-        # vid = cv2.VideoCapture('{}/{}'.format(vloc, videos))
-        # print(vid, 'Images for video.: ', videos)
-        # # i = 0
-        # skip = 10
-        # print("Video Processing")
-        # while True:
-        #     i += 1
-        #     grabbed, t1 = vid.read()
-        #     if not grabbed:
-        #         break
-        #     if i % skip:
-        #         continue
-        #     t1 = preprocess(t1)
-        #     print(i)
-        #     cv2.imwrite('{}/{}.png'.format(imgloc, i), t1)
+        vid = cv2.VideoCapture('{}/{}'.format(vloc, videos))
+        print(vid, 'Images for video.: ', videos)
+        # i = 0
+        skip = 10
+        print("Video Processing")
+        while True:
+            i += 1
+            grabbed, t1 = vid.read()
+            if not grabbed:
+                break
+            if i % skip:
+                continue
+            t1 = preprocess(t1)
+            print(i)
+            cv2.imwrite('{}/{}.png'.format(imgloc, i), t1)
 
-        # vid.release()
+        vid.release()
